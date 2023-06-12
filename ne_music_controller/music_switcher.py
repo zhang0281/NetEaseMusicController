1c5b9032121e22005c159fca2204f8a6ae5ab043
zhang <qq814608@163.com>
2023-06-12 17:33:22 +0800

    feature(main): 功能修复 1. 将autopy替换为pywinauto。可以不使用全局快捷键。 2. 注释掉了关机功能。 3. 添加了喜欢音乐功能。

--- ne_music_controller/music_switcher.py
+++ ne_music_controller/music_switcher.py
@@ -1,21 +1,16 @@
 # coding: utf-8
 import os
+
+import psutil
 from flask import Flask, request, jsonify
-import autopy
+from pywinauto import Application
 
-from autopy.key import (
-    toggle,
-    Modifier,
-    Code,
-)
-
-K_CONTROL = Modifier.CONTROL
-K_F3 = Code.F3
-K_F1 = Code.F1
-K_F2 = Code.F2
-K_F4 = Code.F4
-K_F6 = Code.F6
-
+pause_play = "^P"
+prev_song = "^{LEFT}"
+next_song = "^{Right}"
+volume_up = "^{UP}"
+volume_down = "^{DOWN}"
+like_song = "^L"
 
 app = Flask(__name__)
 project_root = os.path.abspath(".")
@@ -23,36 +18,53 @@ template = open(os.path.join(project_root, "templates/index.html"), "r", encodin
 
 
 class MusicBox(object):
+
+    @classmethod
+    def check_if_running(cls, process_name):
+        for proc in psutil.process_iter(['name']):
+            if proc.info['name'] == process_name:
+                return True
+        return False
+
     @classmethod
     def _press_keys(cls, keys):
-        autopy.key.tap(
-            keys[1],
-            [keys[0]]
-        )
+        if cls.check_if_running(process_name='cloudmusic.exe'):
+            # 自动从路径打开网易云音乐
+            Application(backend="uia").start(r"C:\Program Files (x86)\NetEase\CloudMusic\cloudmusic.exe")
+        # 连接打开网易云音乐的窗口
+        app_connect = Application(backend='uia').connect(class_name="OrpheusBrowserHost")
+        # 获取顶层窗口
+        top_window = app_connect.top_window()
+        # 输入指定按键
+        top_window.type_keys(keys)
 
     @classmethod
     def next_song(cls):
-        cls._press_keys((K_CONTROL, K_F3))
+        cls._press_keys(next_song)
 
     @classmethod
     def prev_song(cls):
-        cls._press_keys((K_CONTROL, K_F1))
+        cls._press_keys(prev_song)
 
     @classmethod
     def pause_play(cls):
-        cls._press_keys((K_CONTROL, K_F2))
+        cls._press_keys(pause_play)
 
     @classmethod
     def volume_up(cls):
-        cls._press_keys((K_CONTROL, K_F6))
+        cls._press_keys(volume_up)
 
     @classmethod
     def volume_down(cls):
-        cls._press_keys((K_CONTROL, K_F4))
+        cls._press_keys(volume_down)
 
     @classmethod
-    def shutdown(cls):
-        os.system("shutdown -s -t 5")
+    def like_song(cls):
+        cls._press_keys(like_song)
+
+    # @classmethod
+    # def shutdown(cls):
+    #     os.system("shutdown -s -t 5")
 
 
 @app.route('/')
