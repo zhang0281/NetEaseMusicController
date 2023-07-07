f2622fa24f13377095df95a0d1b73ca4f04a346b
zhang <qq814608@163.com>
2023-07-07 23:44:52 +0800

    feature: 功能修改 1. 需求安装BetterNCM和其中的InfLink插件的网易云音乐。主要是需求SMTC协议。 2. 使用SMTC协议来获取歌曲信息以及控制播放或者音量。 3. 可获取曲名，歌手和封面。 4. 音量修改更改为滑条。 5. 更新bootstrap为bootstrap5。 6. 退出时自动恢复音量为100%。

--- ne_music_controller/music_switcher.py
+++ ne_music_controller/music_switcher.py
@@ -1,21 +1,20 @@
 # coding: utf-8
-
-# https://pywinauto.readthedocs.io/en/latest/code/pywinauto.keyboard.html?highlight=pywinauto.keyboard.html#module-pywinauto.keyboard
-# https://pywinauto.readthedocs.io/en/latest/HowTo.html#how-to-specify-a-usable-application-instance
-
+import asyncio
+import atexit
+import json
 import os
-import time
-
-import psutil
-from flask import Flask, request, jsonify
-from pywinauto import Application
-
-pause_play = "^P"
-prev_song = "^{LEFT}"
-next_song = "^{RIGHT}"
-volume_up = "^{UP}"
-volume_down = "^{DOWN}"
-like_song = "^L"
+from io import BytesIO
+import traceback
+import winsdk.windows.media.control as wmc
+from comtypes import CoInitialize, CoUninitialize
+from flask import Flask, request, jsonify, send_file
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+from win32api import keybd_event
+from win32con import VK_MEDIA_PLAY_PAUSE, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK, KEYEVENTF_EXTENDEDKEY
+from winsdk.windows.storage.streams import (DataReader, Buffer, InputStreamOptions)
+import logging
+import io
+import PIL.Image
 
 app = Flask(__name__)
 project_root = os.path.abspath(".")
@@ -23,65 +22,157 @@ template = open(os.path.join(project_root, "templates/index.html"), "r", encodin
 
 
 class MusicBox(object):
+    @classmethod
+    def image2png(cls, rawdata):
+        ''' convert an image to png '''
+
+        if not rawdata:
+            return None
+
+        if rawdata.startswith(b'\211PNG\r\n\032\n'):
+            logging.debug('already PNG, skipping convert')
+            return rawdata
+
+        try:
+            imgbuffer = io.BytesIO(rawdata)
+            logging.getLogger('PIL.TiffImagePlugin').setLevel(logging.CRITICAL + 1)
+            logging.getLogger('PIL.PngImagePlugin').setLevel(logging.CRITICAL + 1)
+            image = PIL.Image.open(imgbuffer)
+            imgbuffer = io.BytesIO(rawdata)
+            if image.format != 'PNG':
+                image.convert(mode='RGB').save(imgbuffer, format='PNG')
+        except Exception as error:  # pylint: disable=broad-except
+            logging.debug(error)
+            return None
+        logging.debug("Leaving image2png")
+        return imgbuffer.getvalue()
 
     @classmethod
-    def check_if_running(cls, process_name):
-        for proc in psutil.process_iter(['name']):
-            if proc.info['name'] == process_name:
-                return True
-        return False
+    async def _getcoverimage(cls, thumbref):
+        ''' read the thumbnail buffer '''
+        try:
+            thumb_read_buffer = Buffer(5000000)
+
+            readable_stream = await thumbref.open_read_async()
+            await readable_stream.read_async(thumb_read_buffer, thumb_read_buffer.capacity,
+                                       InputStreamOptions.READ_AHEAD)
+            buffer_reader = DataReader.from_buffer(thumb_read_buffer)
+            if byte_buffer := bytearray(
+                    buffer_reader.read_buffer(buffer_reader.unconsumed_buffer_length)):
+                return cls.image2png(byte_buffer)
+        except:  # pylint: disable=bare-except
+            for line in traceback.format_exc().splitlines():
+                logging.error(line)
+        return None
 
     @classmethod
-    def _press_keys(cls, keys):
-        if not cls.check_if_running(process_name='cloudmusic.exe'):
-            # 自动从路径打开网易云音乐
-            Application(backend="uia").start(r"C:\Program Files (x86)\NetEase\CloudMusic\cloudmusic.exe")
-            time.sleep(5)
-        # 连接打开网易云音乐的窗口
-        app_connect = Application(backend='uia').connect(class_name="OrpheusBrowserHost")
-        # 获取顶层窗口
-        top_window = app_connect.top_window()
+    async def get_media_info(cls):
+        sessions = await wmc.GlobalSystemMediaTransportControlsSessionManager.request_async()
 
-        top_window.wait('ready')
-        # 输入指定按键
-        top_window.type_keys(keys)
+        # This source_app_user_model_id check and if statement is optional
+        # Use it if you want to only get a certain player/program's media
+        # (e.g. only chrome.exe's media not any other program's).
+
+        # To get the ID, use a breakpoint() to run sessions.get_current_session()
+        # while the media you want to get is playing.
+        # Then set TARGET_ID to the string this call returns.
+
+        global current_session
+        current_session = sessions.get_current_session()
+        if current_session:  # there needs to be a media session running
+            if current_session.source_app_user_model_id.startswith('cloudmusic'):
+                info = await current_session.try_get_media_properties_async()
+
+                # song_attr[0] != '_' ignores system attributes
+                info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if
+                             song_attr[0] != '_'}
+
+                # converts winrt vector to list
+                info_dict['genres'] = list(info_dict['genres'])
+
+                return info_dict
+
+        # It could be possible to select a program from a list of current
+        # available ones. I just haven't implemented this here for my use case.
+        raise Exception('TARGET_PROGRAM is not the current media session')
+
+    @classmethod
+    def get_info(cls):
+        data = asyncio.run(cls.get_media_info()).copy()
+        del data['thumbnail']
+        return json.dumps(data)
+
+    @classmethod
+    async def get_info_pic(cls):
+
+        thumbnail = (await cls.get_media_info())['thumbnail']
+        result = await cls._getcoverimage(thumbnail)
+        return send_file(BytesIO(result), mimetype='image/png')
+
+    @classmethod
+    def get_pic(cls):
+        return asyncio.run(cls.get_info_pic())
+
+    @classmethod
+    def media_is(cls, state):
+        if current_session is None:
+            return False
+        # CHANGING CLOSED OPENED PAUSED PLAYING STOPPED
+        return int(wmc.GlobalSystemMediaTransportControlsSessionPlaybackStatus[
+                       state]) == current_session.get_playback_info().playback_status  # get media state enum and compare to current main media session state
 
     @classmethod
     def next_song(cls):
-        cls._press_keys(next_song)
+        keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
 
     @classmethod
     def prev_song(cls):
-        cls._press_keys(prev_song)
+        keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
 
     @classmethod
     def pause_play(cls):
-        cls._press_keys(pause_play)
+        keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0)
 
     @classmethod
-    def volume_up(cls):
-        cls._press_keys(volume_up)
+    def vol_slider(cls, arg):
+        if isinstance(arg, str):
+            arg = float(arg)
+        CoInitialize()
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if session.Process and session.Process.name().startswith("cloudmusic"):
+                print("cloudmusic volume: %s" % str(round(volume.GetMasterVolume(), 2)))
+                volume.SetMasterVolume(arg, None)
+        CoUninitialize()
 
     @classmethod
-    def volume_down(cls):
-        cls._press_keys(volume_down)
-
-    @classmethod
-    def like_song(cls):
-        cls._press_keys(like_song)
-
-    # @classmethod
-    # def shutdown(cls):
-    #     os.system("shutdown -s -t 5")
+    def get_vol(cls):
+        CoInitialize()
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if session.Process and session.Process.name().startswith("cloudmusic"):
+                return round(volume.GetMasterVolume(), 2)
+        CoUninitialize()
 
 
 @app.route('/')
 def hello_world():
     key_word = request.args.get("action")
+    arg = request.args.get("arg")
+    result = None
     if key_word:
-        getattr(MusicBox, key_word)()
-
-    return template
+        if arg:
+            result = getattr(MusicBox, key_word)(arg)
+        else:
+            result = getattr(MusicBox, key_word)()
+    if result is not None:
+        if isinstance(result, float) or isinstance(result, dict) :
+            return str(result)
+        return result
+    else:
+        return template
 
 
 # 移动端连接服务器校验
@@ -95,3 +186,9 @@ def mobile_connect():
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10010)
 
+
+def restore_volume():
+    MusicBox.vol_slider(1)
+
+
+atexit.register(restore_volume)
